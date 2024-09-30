29aa288862fc0ff1e8aea5a6cdb8f45aeed9b4f5
Francisco Posada <ciscotomaramo@gmail.com>
2024-09-30 11:52:45 +0100

    Views advanced, issue with routes resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'cash_registers#index'
-
+  root 'cash_registers#menu'
+  get 'items', to: 'cash_registers#index'
+  get 'discounts', to: 'cash_registers#discounts'
+  get 'cart', to: 'cash_registers#cart'
+  get 'checkout', to: 'cash_registers#checkout'
   post 'add_item', to: 'cash_registers#add_item', as: 'add_item'
-  get 'cart', to: 'cash_registers#cart', as: 'cart'
-  get 'checkout', to: 'cash_registers#checkout', as: 'checkout'
 end
